8b263af7254c8474ae9ac96601744c5a5e816cbf
Nick Barry <niko.barry@gmail.com>
2016-07-05 14:37:15 -0700

    Add ability to mark contact as "just contacted"
    Clicking on the 'just contacted button' marks the contact as contacted today, and updates the relevant contact details.
    
    I'm using the 'moment' package for easier date handling

--- client/app/app.js
+++ client/app/app.js
@@ -1,25 +1,41 @@
+//var moment = require('moment');
+
 angular.module('getintouch', [])
-.controller('RemindersController', function($scope){
-  $scope.reminders = [
+.controller('ContactsController', function($scope){
+  $scope.contacts = [
     {
       name: 'Elyse Green',
-      lastContacted: new Date(),
-      contactFrequency: 1
+      lastContacted: new moment(),
+      lastContactedLabel: new moment().format('MMM D, YYYY'),
+      contactFrequency: 1,
+      contactNext: new moment("2016-07-06")
     },
     {
       name: 'Chris Brenton',
-      lastContacted: new Date("2016-06-30"),
-      contactFrequency: 14
+      lastContacted: new moment("2016-06-30"),
+      lastContactedLabel: new moment("2016-06-30").format('MMM D, YYYY'),
+      contactFrequency: 14,
+      contactNext: new moment("2016-07-13")
     },
     {
-      name: 'Rob Hines',
-      lastContacted: new Date("2016-02-20"),
-      contactFrequency: 180
+      name: 'Catrina Fuentes',
+      lastContacted: new moment("2015-02-20"),
+      lastContactedLabel: new moment("2015-02-20").format('MMM D, YYYY'),
+      contactFrequency: 180,
+      contactNext: new moment("2015-08-20")
     },
     {
       name: 'Nick Winter',
-      lastContacted: new Date("2015-10-30"),
-      contactFrequency: 365
+      lastContacted: new moment("2015-01-30"),
+      lastContactedLabel: new moment("2015-01-30").format('MMM D, YYYY'),
+      contactFrequency: 365,
+      contactNext: new moment("2016-01-30")
     }
   ];
+
+  $scope.markAsContacted = function(contact){
+    contact.lastContacted = new moment(); // Record today's contact
+    contact.lastContactedLabel = contact.lastContacted.format('MMM D, YYYY');
+    contact.contactNext.add(contact.contactFrequency, 'days'); // update .contactNext property
+  }
 })
